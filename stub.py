1392fda5f585d8b0486029c334218396ae215851
Bryan Lee <bryanlee99@gmail.com>
2019-05-07 00:03:41 -0400

    Final monkey.

--- stub.py
+++ stub.py
@@ -3,41 +3,41 @@ import numpy as np
 import numpy.random as npr
 import pygame as pg
 import util
-import random
 
 from SwingyMonkey import SwingyMonkey
 
-
-
 class Learner(object):
     '''
-    This agent jumps randomly.
+    This agent plays the swinging monkey game
     '''
-
-    def __init__(self, it, round_size):
-        self.last_state  = None
-        self.last_state_unformat = None
-        self.last_action = None
-        self.last_reward = None
-        self.curr_gravity = 0
-        self.epoch = 1
-        self.round_size = round_size
-        self.it = it
-        self.values = util.Counter()
-        self.epsilon = [0.2 * self.transformSigmoid(self.epoch)]
-        self.death_causes = {-10:[], -5:[]}
-
+    def __init__(self, it, round_size, gamma, learning_rate):
+        self.last_state  = None         # Previous state
+        self.last_state_unformat = None # Previous state, unformatted
+        self.last_action = None         # Last action
+        self.last_reward = None         # Last reward
+        self.curr_gravity = 0           # Gravity in this stage
+        self.epoch = 1                  # Number of epochs covered
+        self.round_size = round_size    # Number of pixels to round states to
+        self.it = it                    # Number of iterations run by alg
+        self.values = util.Counter()    # Q values
+        self.epsilon = [0]              #Surprinsgly, no exploration works best | Prev Exploration: [0.2 * self.transformSigmoid(self.epoch)]
+        self.death_causes = {-10:[], -5:[]} # Monkey bot height at each death
+        self.gamma = gamma              # Discount factor
+        self.learning_rate = learning_rate  # Learning Rate
+        self.state_counter = util.Counter() # Number of times a state has been encountered
 
     def reset(self):
+        # Resets some variables
         self.last_state  = None
         self.last_state_unformat = None
         self.last_action = None
         self.last_reward = None
         self.curr_gravity = None
-        self.epoch += 1
-        self.epsilon.append(0.2 * self.transformSigmoid(self.epoch))
-        #print(self.death_causes)
 
+        # Iterate epoch
+        self.epoch += 1
+
+    # Caller functions
     def getValues(self):
         return self.values
 
@@ -47,32 +47,25 @@ class Learner(object):
     def getDeathCauses(self):
         return self.death_causes
 
+    # Transforms sigmoid function for exploration
     def transformSigmoid(self, it):
         x = self.it/1.1 - it
         sig = np.exp(x)/(np.exp(x) + 1)
         return sig
 
-    # in order: tree dist, tree top, tree bot, monkey vel, monkey top, monkey bot
+    # Returns features describing a state
     def format_into_tuple(self, state_dict, gravity):
-        # return (state_dict['tree']['dist'], state_dict['tree']['top'],\
-        #     state_dict['tree']['bot'], state_dict['monkey']['vel'],\
-        #     state_dict['monkey']['top'], state_dict['monkey']['bot'])
-
         danger_high = state_dict['monkey']['top'] > 350
         danger_low = state_dict['monkey']['bot'] < 50
-        # danger_high = 0
-        # danger_low = 0
         return (state_dict['tree']['dist'] // self.round_size, (state_dict['tree']['top'] - state_dict['monkey']['top']) // self.round_size, \
                 gravity, danger_high, danger_low)
 
+    # Returns the desired action
     def action_callback(self, state):
-        '''
-        Implement this function to learn things and take actions.
-        Return 0 if you don't want to jump and 1 if you do.
-        '''
+        # Get hyperparameters
         epsilon = self.epsilon[-1]
-        gamma = 1
-        learning_rate = 0.3
+        gamma = self.gamma
+        learning_rate = self.learning_rate
 
         # Infer Gravity
         if self.curr_gravity == None and self.last_state_unformat != None:
@@ -88,9 +81,12 @@ class Learner(object):
         else:
             state_formatted = self.format_into_tuple(state, self.curr_gravity)
 
+        # Determine action via Q-values and learn Q-values
         if self.last_action is not None:
             last_Q = self.values[self.last_state, self.last_action]
-            if self.values[state_formatted, 1] > self.values[state_formatted, 0]:
+            left = self.values[state_formatted, 1]
+            right = self.values[state_formatted, 0]
+            if left > right:
                 next_val = self.values[state_formatted, 1]
                 next_action = 1
             else:
@@ -108,17 +104,19 @@ class Learner(object):
             else:
                 self.last_action = 1
 
+        # Update Counter for number of times a state has been encountered
+        if (state_formatted, self.last_action) in self.state_counter.keys():
+            self.state_counter[(state_formatted, self.last_action)] += 1
+        else:
+            self.state_counter[(state_formatted, self.last_action)] = 1
+
         self.last_state = state_formatted
         self.last_state_unformat = state
         return self.last_action
 
+    # Returns the reward
     def reward_callback(self, reward):
-        '''This gets called so you can see what reward you get.'''
         self.last_reward = reward
-        if(reward < 0):
-            self.death_causes[reward].append(self.last_state_unformat['monkey']['bot'])
-            #print(reward)
-            #print(np.mean(self.death_causes[reward]))
 
 def run_games(learner, hist, iters = 100, t_len = 10):
     '''
@@ -145,72 +143,44 @@ def run_games(learner, hist, iters = 100, t_len = 10):
     pg.quit()
     return
 
-round_size = [10, 50, 100, 150, 200]
-round_size = [100]
-if __name__ == '__main__':
 
+if __name__ == '__main__':
+    # Hyper parameters and global vars
+    round_size = [100]
+    learning_rate = [0.1]
+    gamma = [1]
     values = util.Counter()
     death_causes = util.Counter()
     scores = {}
-    for r in round_size:
-        repeats = 50
-        it_in_repeat = 100
-        curr_round_score = []
-        for i in range(repeats):
-            # Select agent.
-            agent = Learner(it_in_repeat, r)
 
-            # Empty list to save history.
-            hist = []
+    # Runs iterations of monkey for 30 iterations, and 300 epochs within each iteration
+    for l in learning_rate:
+        for g in gamma:
+            for r in round_size:
+                repeats = 30
+                it_in_repeat = 300
+                curr_round_score = []
+                for i in range(repeats):
+                    # Select agent.
+                    agent = Learner(it_in_repeat, r, g, l)
 
-            # Run games.
-            run_games(agent, hist, it_in_repeat, 1)
+                    # Empty list to save history.
+                    hist = []
 
-            # Add to values counter
-            values += agent.getValues()
+                    # Run games.
+                    run_games(agent, hist, it_in_repeat, 1)
 
-            # # Get Epsilon
-            # import matplotlib.pyplot as plt
-            #
-            # plt.figure()
-            # plt.plot(agent.getEpsilon())
-            # plt.show()
+                    # Add to values counter
+                    values += agent.getValues()
 
-            # Score Avg
-            curr_round_score.append(hist)
+                    # Score Avg
+                    curr_round_score.append(hist)
 
-            # Print Death Causes
-            death_causes += agent.getDeathCauses()
+                    # Print Death Causes
+                    death_causes += agent.getDeathCauses()
 
-            # Save history.
-            np.save('hist',np.array(hist))
-        scores[r] = curr_round_score
-    # Get Values
-    # import matplotlib.pyplot as plt
-    # plt.figure()
-    # plt.hist(list(values.values()), bins = np.arange(-10, 10, 1))
-    # plt.title("Q Value Distribution")
-    # plt.show()
+                    # Save history.
+                    np.save('hist',np.array(hist))
+                scores[(r, g, l)] = curr_round_score
 
-import matplotlib.pyplot as plt
-# # Death Causes: Show distribution of death locations
-# plt.figure()
-# plt.hist(death_causes[-10])
-# plt.title("Location of Character (bot) before Death")
-# plt.show()
-
-
-for r in round_size:
-    curr_scores = np.asarray(scores[r])
-    print(scores)
-
-    print_scores = curr_scores.flatten()
-    print_scores = [max(j) for j in curr_scores]
-    plt.figure()
-    bins = np.arange(0, max(print_scores) + 2, 1)
-    plt.hist(print_scores, bins = bins)
-    plt.xticks(np.arange(0, max(print_scores) + 2, 1))
-    plt.title('Max Scores Across Iterations for Rounding ' + str(r))
-    plt.show()
-    print("hello")
 
